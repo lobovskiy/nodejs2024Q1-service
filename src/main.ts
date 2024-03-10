d238aac4a78dd70c0e4d8fe5c5a75660e74050be
Artem Lobovskii <lobovskiy@yandex.ru>
2024-03-10 14:36:21 +0300

    feat: implement user module

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,15 @@ import { format } from 'path';
 import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
 import { getYamlDocument } from './app.utils';
+import { ValidationPipe } from '@nestjs/common';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
   const swaggerYamlDocumentPath = format({
     root: '/',
     dir: process.env.DOC_FOLDER_NAME || 'doc',
