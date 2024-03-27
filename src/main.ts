aeb3936c8e731f071d16bee5ea165939fe0da2b2
Artem Lobovskii <lobovskiy@yandex.ru>
2024-03-28 01:52:55 +0300

    feat: add handling uncaught exception and unhandled rejection

--- src/main.ts
+++ src/main.ts
@@ -20,14 +20,24 @@ async function bootstrap() {
     logger: ['error', 'warn', 'log'],
   });
   const swaggerDocument = await getYamlDocument(YAML_DOC_PATH);
+  const loggingService = app.get(LoggingService);
   const logsFolderPath = getCwdPath(LOGS_FOLDER_PATH);
 
   SwaggerModule.setup(SWAGGER_MODULE_PATH, app, swaggerDocument);
 
   await ensureFolderExists(logsFolderPath);
 
+  process.on('uncaughtException', (error) => {
+    loggingService.logUncaughtException(error);
+    process.exit(1);
+  });
+  process.on('unhandledRejection', (reason) => {
+    loggingService.logUnhandledRejection(reason);
+    process.exit(1);
+  });
+
   app.useGlobalPipes(new ValidationPipe());
-  app.useLogger(app.get(LoggingService));
+  app.useLogger(loggingService);
   await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
